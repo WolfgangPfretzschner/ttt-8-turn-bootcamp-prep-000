f0fae679857d5f5d87bcbef2318fd28fe761d463
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-04-10 13:25:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,3 +23,12 @@ end
 def turn()
   
 end
+
+def position_taken?(board, index)
+  
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"  
+    return true
+  end
+end
